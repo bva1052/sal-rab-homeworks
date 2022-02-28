3bce8ec5557077b53eecbafffe2f4e77841205d0
Viktoriya Belik <bva1052@yandex.ru>
2022-02-28 19:23:35 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,24 @@
 //    }
 // }
 
+
+
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
 
+    client = name + ' ' + phone;
+    address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
+ 
+    let data = {client, order: {address, sum}, goods: []};
+ 
     let countOfGoods = goods.length;
+    let goodsP = [];
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i ++) {
+        goodsP[i] = {title: goods[i].title, count: goods[i].count};
+    
     }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    data.goods = goodsP;
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
